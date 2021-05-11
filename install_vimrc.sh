9b9033b8512d9c05bee32751f42e0f60c0a5b9e0
Sartaj Singh Wariah <sartaj_singh_wariah@dell.com>
2021-05-11 14:04:31 +0530

    Make fonts directory if it doesn't exist

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -20,6 +20,7 @@ wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20R
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+sudo mkdir -p ~/.local/share/fonts
 sudo cp MesloLGS* ~/.local/share/fonts
 rm MesloLGS*
 
